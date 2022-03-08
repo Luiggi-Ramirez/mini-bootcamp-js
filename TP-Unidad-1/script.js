7226edb84559a28a7193784587b78677fdf3de37
Luiggi <luirami98@gmail.com>
2022-03-07 21:17:01 -0500

    TP Unidad 1

---
+++ TP-Unidad-1/script.js
@@ -0,0 +1,8 @@
+// a) Mostrar el día de tu cumpleaños (en numero) Ej: 23
+console.log(26)
+// b) Mostrar el mes de tu cumpleaños (en numero) Ej: 07
+console.log(03)
+// c) Mostrar el año de tu nacimiento.
+console.log(1998)
+// d) Mostrar tu número favorito.
+console.log(10)
