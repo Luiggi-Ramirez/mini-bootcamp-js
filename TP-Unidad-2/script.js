65435c985f7d73bd9b5ba0c2ec3632e1976aac75
Luiggi <luirami98@gmail.com>
2022-03-07 21:31:07 -0500

    TP-Unidad-2

---
+++ TP-Unidad-2/script.js
@@ -0,0 +1,18 @@
+// Declarar una variable de nombre cantidadDeVentanasEnMiCasa y asignarle la cantidad de ventanas que hay en tu casa. Mostrar el valor de la variable por la consola.
+var cantidadDeVentanasEnMiCasa = 2
+console.log(cantidadDeVentanasEnMiCasa)
+// Declarar una variable de nombre soyHumano y asignarle el valor true (verdadero). Mostrar el valor de la variable por la consola.
+var soyHumano = true
+console.log(soyHumano)
+// Declarar una variable de nombre miGustoDePizzaPreferido y asignarle la variedad de pizza que más te guste usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola.
+var miGustoDePizzaPreferido = "Con piña y bastante queso"
+console.log(miGustoDePizzaPreferido)
+// Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable.
+var mostrar = false
+console.log(mostrar)
+mostrar = "Hola"
+console.log(mostrar)
+mostrar = 0
+console.log(mostrar)
+mostrar = "Minios"
+console.log(mostrar)
